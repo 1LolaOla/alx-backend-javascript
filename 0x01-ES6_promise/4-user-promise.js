49fcb79d134491f1e15bb4e9c9198775d0ffaf27
1LolaOla <funmiolay1@gmail.com>
2023-07-25 20:07:10 +0000

    4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,5 @@
 function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName: firstName,
-    lastName: lastName,
-  });
+  return Promise.resolve({ firstName, lastName });
 }
 
 export default signUpUser;
