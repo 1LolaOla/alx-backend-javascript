41b8b83b3acc7d84e190dfbbf07c06c77ecbdb20
1LolaOla <funmiolay1@gmail.com>
2023-08-01 13:09:55 +0000

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,3 @@
-// 4-update_grade_by_city.js
-
 /**
  * Updates student grades for a specific city based on the given newGrades array.
  * @param {Array} students - Array of student objects.
@@ -9,14 +7,10 @@
  */
 function updateStudentGradeByCity(students, city, newGrades) {
   return students.map((student) => {
-    const matchedGrade = newGrades.find((grade) => grade.studentId === student.id);
-    if (matchedGrade) {
-      return { ...student, grade: matchedGrade.grade };
-    } else {
-      return { ...student, grade: 'N/A' };
-    }
+    const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+    const grade = gradeObj ? gradeObj.grade : 'N/A';
+    return { ...student, grade };
   }).filter((student) => student.location === city);
 }
 
 export default updateStudentGradeByCity;
-
