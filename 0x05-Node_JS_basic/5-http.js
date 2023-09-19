735ce49d3213871895fa3dca8cfcf3d8db116bc5
1LolaOla <funmiolay1@gmail.com>
2023-09-19 22:56:20 +0000

    5-http.js

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async.js');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  }
+  if (req.url === '/students') {
+    const path = process.argv[2];
+    countStudents(path).then((resp) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      const response = `This is the list of our students\n${resp}`;
+      res.end(response);
+    }).catch(() => {
+      res.statusCode = 404;
+      res.end('This is the list of our students\nCannot load the database');
+    });
+  }
+});
+
+app.listen(1245, 'localhost');
+
+module.exports = app;
