4c2ca5d1382ba283988d4f9c810bdeb8d244119d
1LolaOla <funmiolay1@gmail.com>
2023-09-21 16:50:15 +0000

    api.js

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,22 +1,15 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
-});
-
-app.get(/^\/cart\/(\d+)$/, (req, res) => {
-  const id = req.params[0];
-
-  res.send(`Payment methods for cart ${id}`);
-});
-
 const port = 7865;
 
-app.listen(port, 'localhost', () => {
-  console.log(`API avalilable on localhost port ${port}`);
+app.get('/', (req, res) => {
+  res.end('Welcome to the payment system');
 });
 
-module.exports = app;
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.end(`Payment methods for cart ${req.params.id}`);
+});
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
