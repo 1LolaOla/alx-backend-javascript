23d38275d2d73d49db332a1c9d9aeeca12d49453
1LolaOla <funmiolay1@gmail.com>
2023-09-21 16:13:37 +0000

    2-calcul_chai.test.js

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,34 +1,67 @@
-const chai = require('chai');
-
-const { expect } = chai;
+const assert = require('assert');
+const { expect } = require('chai');
+const { it, describe, test } = require('mocha');
 const calculateNumber = require('./2-calcul_chai');
 
-describe('2-calcul_chai.calculateNumber()', () => {
-  it('should return 6 when type is SUM, a is 1.4 and b is 4.5', () => {
-    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+describe('calculateNumber', () => {
+    describe('SUM', () => {
+      it('checking if numbers round', () => {
+        expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+      });
+      it('checking if numbers round 2nd round', () => {
+        expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
+      });
+      it('checking if numbers round 3rd round', () => {
+        expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+      });
+      it('checking if numbers round 4th round', () => {
+        expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
+      });
+      it('checking if numbers round 5th round', () => {
+        expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+      });
+      it('checking negative return', () => {
+        expect(calculateNumber('SUM', -1.3, -3.7)).to.equal(-5);
+      });
+    });
+    describe('SUBTRACT', () => {
+      it('checking if numbers round', () => {
+        expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
+      });
+      it('checking if numbers round 2nd round', () => {
+        expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
+      });
+      it('checking if numbers round 3rd round', () => {
+        expect(calculateNumber('SUBTRACT', 6.2, 3.7)).to.equal(2);
+      });
+      it('checking if numbers round 4th round', () => {
+        expect(calculateNumber('SUBTRACT', 5.5, 3.7)).to.equal(2);
+      });
+      it('checking if numbers round 5th round', () => {
+        expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+      });
+      it('checking negative return', () => {
+        expect(calculateNumber('SUBTRACT', -1.3, -3.7)).to.equal(3);
+      });
+    });
+    describe('DIVIDE', () => {
+      it('checking if numbers round', () => {
+        expect(calculateNumber('DIVIDE', 3, 1)).to.equal(3);
+      });
+      it('checking if numbers round 2nd round', () => {
+        expect(calculateNumber('DIVIDE', 4, 3.7)).to.equal(1);
+      });
+      it('checking if numbers round 3rd round', () => {
+        expect(calculateNumber('DIVIDE', 2.2, 0.7)).to.equal(2);
+      });
+      it('checking if numbers round 4th round', () => {
+        expect(calculateNumber('DIVIDE', 15, 2.7)).to.equal(5);
+      });
+      it('checking if numbers round 5th round', () => {
+        expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+      });
+      it('checking negative return', () => {
+        expect(calculateNumber('DIVIDE', -1.3, 0)).to.equal('Error');
+      });
+    });
   });
-
-  it('should return -4 when type is SUBTRACT, a is 1.4 and b is 4.5', () => {
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-  });
-
-  it('should return 0.2 when type is DIVIDE, a is 1.4 and b is 4.5', () => {
-    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-  });
-
-  it('should return `Error` when type is DIVIDE, a is 1.4 and b is 0', () => {
-    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
-  });
-
-  it('should return undefined when type is not SUM, DIVIDE or Subtract, a is 1.4 and b is 0', () => {
-    expect(calculateNumber({}, 1.4, 0)).to.equal(null);
-  });
-
-  it('should return NaN when type is SUM, a is {} and b is 0', () => {
-    expect(Number.isNaN(calculateNumber('SUM', {}, 0))).to.be.true;
-  });
-
-  it('should throw reference error when given an undefined value', () => {
-    expect(() => { calculateNumber(a, 3, 4); }).to.throw(ReferenceError);
-  });
-});
